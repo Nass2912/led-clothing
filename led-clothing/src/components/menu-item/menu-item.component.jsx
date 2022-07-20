c6dddb864caa227c59dc936c7a7f1dfad142d82d
Soobratty Anas <soobratty.as@gmail.com>
2022-07-20 10:33:49 +0400

    add styling

--- led-clothing/src/components/menu-item/menu-item.component.jsx
+++ led-clothing/src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./menu-item.style.scss";
 
 export const MenuItem = ({title, img, size}) => (
-    <div style = {{
+    <div className= {`menu-item ${size}`}>
+        <div className="background-image" 
+        style = {{
         backgroundImage: `url(${img})`
-    }} className= {`menu-item ${size}`}>
+        }}/>
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
         </div>
     </div>
